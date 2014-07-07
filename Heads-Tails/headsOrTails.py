1049b2ac47a7477cfff5fa35db480f2bb51fa672
Cory McDonald <cory@corywmcdonald.com>
2014-07-07 16:28:23 -0500

    Adding the files

---
+++ Heads-Tails/headsOrTails.py
@@ -0,0 +1,5 @@
+import random
+
+userChoice = ""
+while userChoice == "" and (userChoice != 'heads' or userChoice != "tails"):
+    userChoice = input('Enter heads or tails:')
---
+++ Name Generator/generateName.py
@@ -0,0 +1,41 @@
+import random
+
+#todo add options for generating x amount of names and specifying whethe you want female or male
+
+#Grab male names
+maleFirstNamesFile = open('maleFirstName.txt', 'r')
+maleFirstNames = []
+
+for name in maleFirstNamesFile:
+    maleFirstNames.append(name.split()[0])
+    
+maleFirstNamesFile.close()
+
+#Grab female names
+femaleFirstNamesFile = open('femaleFirstNames.txt', 'r')
+femaleFirstNames = []
+
+for name in femaleFirstNamesFile:
+    femaleFirstNames.append(name.split()[0])
+    
+femaleFirstNamesFile.close()
+
+#Grab last names
+lastNameFile = open('lastnames.txt', 'r')
+lastNames = []
+
+for name in lastNameFile:
+    lastNames.append(name.split()[0])
+    
+lastNameFile.close()
+
+#Generate 10 random names
+for x in range(0,10):
+    if random.uniform(0,1) == 0:
+        firstName = random.choice(maleFirstNames)
+    else:
+        firstName = random.choice(femaleFirstNames)
+    
+    lastName = random.choice(lastNames)
+    
+    print(firstName + " " + lastName)
