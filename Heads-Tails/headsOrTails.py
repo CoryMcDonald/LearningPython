3d9b4005fe02b1154a4c37931850584224179c44
Cory McDonald <cory@corywmcdonald.com>
2014-07-07 16:33:14 -0500

    Finished Heads-Tails project

--- Heads-Tails/headsOrTails.py
+++ Heads-Tails/headsOrTails.py
@@ -3,3 +3,10 @@ import random
 userChoice = ""
 while userChoice == "" and (userChoice != 'heads' or userChoice != "tails"):
     userChoice = input('Enter heads or tails:')
+
+headsOrTails = random.choice(['heads','tails'])
+print('Flipping Coin - ' + headsOrTails)
+if userChoice == headsOrTails:
+    print('You win! :D')
+else:
+    print('You lost :(')
