95b5a07b80905781ee06e587b1d1cdc864f90faa
Cory McDonald <cory@corywmcdonald.com>
2014-07-10 16:39:15 -0500

    Added basis of random sentence generator

---
+++ Psuedorandom Sentence Generator/generate.py
@@ -0,0 +1,21 @@
+#Psuedorandom Sentence Generator
+
+#Basic Sentence
+subject = 'I'
+verb = 'run'
+obj = ''
+complement = ''
+indirectObj ''
+directObj = ''
+
+Sentence = subject + verb
+subject + verb
+#I swim. Joe swims. They swam.
+subject + verb + obj
+#I drive a car. Joe plays the guitar. They ate dinner.
+subject + verb + complement
+#I am busy. Joe became a doctor. They look sick.
+subject + verb + indirectObj + directObj
+#I gave her a gift. She teaches us English.
+subject + verb + obj + complement
+#I left the door open. We elected him president. They named her Jane.
